2eddca3323c31161e56f7d387a8357943678e046
Janadesomer1 <jana.desomer@student.howest.be>
2018-11-29 15:26:46 +0100

    audio analyses within three.js now

--- js/microphone/audioAnalyser.js
+++ js/microphone/audioAnalyser.js
@@ -1,111 +1,88 @@
-// Empty fallback for Older Browsers
-if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {};
-}
+// // Empty fallback for Older Browsers
+// if (navigator.mediaDevices === undefined) {
+//   navigator.mediaDevices = {};
+// }
 
-// add the getUserMedia property when missing
-if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = constraints => {
+// // add the getUserMedia property when missing
+// if (navigator.mediaDevices.getUserMedia === undefined) {
+//   navigator.mediaDevices.getUserMedia = constraints => {
 
-    // Call getUserMedia, if present
-    const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+//     // Call getUserMedia, if present
+//     const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-    // Rejected Promise for browsers who don't accept
-    if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-    }
+//     // Rejected Promise for browsers who don't accept
+//     if (!getUserMedia) {
+//       return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+//     }
 
-    // wrap call to old navigator.getUserMedia with Promise
-    return new Promise((resolve, reject) => {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
-  }
-}
+//     // wrap call to old navigator.getUserMedia with Promise
+//     return new Promise((resolve, reject) => {
+//       getUserMedia.call(navigator, constraints, resolve, reject);
+//     });
+//   }
+// }
 
-// set up forked web audio context, for multiple browsers
-// window. is --> For Safari browser 
-const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-const voiceSelect = document.getElementById("voice");
-let source;
-let stream;
+// // set up forked web audio context, for multiple browsers
+// // window. is --> For Safari browser 
+// const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+// const voiceSelect = document.getElementById("voice");
+// let source;
+// let stream;
 
-// mute button puts microphone on silent 
-const mute = document.querySelector('.mute');
+// // mute button puts microphone on silent 
+// const mute = document.querySelector('.mute');
 
-//set up the different audio nodes we will use for the app
+// //set up the different audio nodes we will use for the app
 
-const analyser = audioCtx.createAnalyser();
-analyser.minDecibels = -90;
-analyser.maxDecibels = -10;
-analyser.smoothingTimeConstant = 0.85;
+// const analyser = audioCtx.createAnalyser();
+// analyser.minDecibels = -90;
+// analyser.maxDecibels = -10;
+// analyser.smoothingTimeConstant = 0.85;
 
-const distortion = audioCtx.createWaveShaper();
-const gainNode = audioCtx.createGain();
-const biquadFilter = audioCtx.createBiquadFilter();
-const convolver = audioCtx.createConvolver();
-
-// grab audio track via XHR for convolver node
-let soundSource;
-ajaxRequest = new XMLHttpRequest();
-ajaxRequest.open('GET', 'https://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
-ajaxRequest.responseType = 'arraybuffer';
-
-ajaxRequest.onload = () => {
-  const audioData = ajaxRequest.response;
-
-  audioCtx.decodeAudioData(audioData, buffer => {
-    soundSource = audioCtx.createBufferSource();
-    convolver.buffer = buffer;
-  }, e => {
-    console.log(`Error with decoding audio data${e.err}`);
-  });
-
-  //soundSource.connect(audioCtx.destination);
-  //soundSource.loop = true;
-  //soundSource.start();
-};
-
-ajaxRequest.send();
+// const distortion = audioCtx.createWaveShaper();
+// const gainNode = audioCtx.createGain();
+// const biquadFilter = audioCtx.createBiquadFilter();
+// const convolver = audioCtx.createConvolver();
 
 
 
-//main block for doing the audio recording
-if (navigator.mediaDevices.getUserMedia) {
-  console.log('getUserMedia supported.');
-  const constraints = {
-    audio: true
-  };
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(
-      stream => {
-        source = audioCtx.createMediaStreamSource(stream);
-        source.connect(analyser);
-        analyser.connect(distortion);
-        distortion.connect(biquadFilter);
-        biquadFilter.connect(gainNode);
-        convolver.connect(gainNode);
-        gainNode.connect(audioCtx.destination);
-        visualize();
-        voiceChange();
-      })
-    .catch(err => {
-      console.log(`The following gUM error occured: ${err}`);
-    })
-} else {
-  console.log('getUserMedia not supported on your browser!');
-}
+// //main block for doing the audio recording
+// if (navigator.mediaDevices.getUserMedia) {
+//   console.log('getUserMedia supported.');
+//   const constraints = {
+//     audio: true
+//   };
+//   navigator.mediaDevices.getUserMedia(constraints)
+//     .then(
+//       stream => {
+//         source = audioCtx.createMediaStreamSource(stream);
+//         source.connect(analyser);
+//         analyser.connect(distortion);
+//         distortion.connect(biquadFilter);
+//         biquadFilter.connect(gainNode);
+//         convolver.connect(gainNode);
+//         gainNode.connect(audioCtx.destination);
+//         //visualize();
+//         //voiceChange();
+//       })
+//     .catch(err => {
+//       console.log(`The following gUM error occured: ${err}`);
+//     })
+// } else {
+//   console.log('getUserMedia not supported on your browser!');
+// }
 
-// event listeners for muting
-mute.onclick = voiceMute;
+// // event listeners for muting
+// mute.onclick = voiceMute;
 
-function voiceMute() {
-  if (mute.id === "") {
-    gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
-    mute.id = "activated";
-    mute.innerHTML = "Unmute";
-  } else {
-    gainNode.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
-    mute.id = "";
-    mute.innerHTML = "Mute";
-  }
-}
+// function voiceMute() {
+//   if (mute.id === "") {
+//     gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
+//     mute.id = "activated";
+//     mute.innerHTML = "Unmute";
+//   } else {
+//     gainNode.gain.setTargetAtTime(1, audioCtx.currentTime, 0)
+//     mute.id = "";
+//     mute.innerHTML = "Mute";
+//   }
+// }
--- js/sceneSubjects/AstronautEnvironment.js
+++ js/sceneSubjects/AstronautEnvironment.js
@@ -1,7 +1,18 @@
 function AstronautEnvironment(scene, constraints) {
 
+    // set up forked web audio context, for multiple browsers
+    // window. is --> For Safari browser
+    const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createAnalyser();
+    //const minDecibels = -90;
+    analyser.minDecibels = -90;
+    // analyser.maxDecibels = -10;
+    // analyser.smoothingTimeConstant = 0.85;
+    let source;
+
     const loader = new THREE.ObjectLoader();
 
+    //astronaut model inladen
     loader.load("./models/model.json",
         function (obj) {
             var light = new THREE.PointLight(0xffffff, 3, 1000);
@@ -9,65 +20,62 @@ function AstronautEnvironment(scene, constraints) {
             obj.position.y += -0.5;
             obj.rotation.x += -1.5;
             scene.add(obj, light);
-        },
-        function (xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% van het astronaut model ingeladen');
-        },
-        function (err) {
-            console.error('astronaut model niet ingeladen');
         }
+        // function (xhr) {
+        //     console.log((xhr.loaded / xhr.total * 100) + '% van het astronaut model ingeladen');
+        // },
+        // function (err) {
+        //     console.error('astronaut model niet ingeladen');
+        // }
     );
 
-    loader.load("./models/environment2.json",
+    //mountain environment model inladen
+    loader.load("./models/mountains.json",
         function (obj) {
             obj.position.y += -2;
             obj.rotation.y += -0.5;
             obj.scale.set(10, 4, 10);
             scene.add(obj);
-        },
-        function (xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% van de environment ingeladen');
-        },
-        function (err) {
-            console.error('astronaut model niet ingeladen');
         }
+        // function (xhr) {
+        //     console.log((xhr.loaded / xhr.total * 100) + '% van de environment ingeladen');
+        // },
+        // function (err) {
+        //     console.error('astronaut model niet ingeladen');
+        // }
     );
 
 
-
+    //microphone opvragen in de browser
     if (navigator.mediaDevices.getUserMedia) {
-        console.log('getUserMedia supported wahoow');
-        const constraints = {
-          audio: true
-        };
-
-
+        console.log('getUserMedia wordt ondersteund wahoow');
+        
         navigator.mediaDevices.getUserMedia(constraints)
           .then(
             stream => {
               source = audioCtx.createMediaStreamSource(stream);
+              source.connect(analyser);
+              analyser.connect(audioCtx.destination);
+              //visualize();
+              
             })
-          .catch(err => {
-            console.log(`The following gUM error occured: ${err}`);
-          })
-      } else {
-        console.log('getUserMedia not supported on your browser!');
-      }
-
-
-
-
-
-        // navigator.mediaDevices.getUserMedia(constraints)
-        //     .then(function(stream) {
-        //         const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-        // })
-        // .catch(function(err) {
-        //     console.log('GetUserMedia could not be called!')
-        // });
+            .catch(err => {
+                console.log(`Ge hebt een gUM foutje: ${err}`);
+                })
+            } else {
+                console.log('getUserMedia werkt niet in deze browser!');
+            }
 
 
     this.update = function (time) {
-        //momenteel nog leeg
+        updateAstronautHeight(time);
+        }
+
+    function updateAstronautHeight(time) {
+        //console.log("deze functie werkt soort van");
+        if(analyser.minDecibels >= -10){
+            console.log("deze functie werkt soort van");
+            obj.rotation.x += -4;
+        }
     }
 }
