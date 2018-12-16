0f94d6d288d19dbbb398c017900ba739e4f6566b
Janadesomer1 <jana.desomer@student.howest.be>
2018-12-16 19:09:20 +0100

    button hover sound works

--- js/buttonSound.js
+++ js/buttonSound.js
@@ -1,11 +1,14 @@
-const main = document.getElementsByClassName("main");
+const startButton = document.getElementById("startButton");
+const storyButton = document.getElementById("storyButton");
 
 const bleep = new Audio();
 bleep.src = "./assets/music/bleep.mp3";
 
-
-main.addEventListener("mouseover", function sound(){
+const handleHoverSound = () => {
     bleep.play();
-    console.log("in eventlistener");
-})
+    console.log("Uwe button word gehovered");
+};
+
+startButton.addEventListener("mouseenter",handleHoverSound);
+storyButton.addEventListener("mouseenter",handleHoverSound);
 
