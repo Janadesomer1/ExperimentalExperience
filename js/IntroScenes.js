0443d746081fd3aa0cfde1c5c5cb0bf5f1c23c28
LisaLapon <lisa.lapon@student.howest.be>
2018-12-17 00:46:39 +0100

    scenes aangemaakt + sound

--- js/IntroScenes.js
+++ js/IntroScenes.js
@@ -2,6 +2,8 @@ const scene = document.getElementById("scene");
 const skip = document.getElementById("skip");
 const aud = document.getElementById("myAudio1");
 const audio2 = document.createElement('audio');
+const audio3 = document.createElement('audio');
+const audio4 = document.createElement('audio');
 
 skip.addEventListener("click", function () {
     skip.remove();
@@ -11,8 +13,8 @@ skip.addEventListener("click", function () {
 const handleEnded = () => {
     console.log("scene1 ended");
     scene.src = "../assets/img/scene2.png";
-    
-    audio2.src = '../assets/music/bleep.mp3';
+
+    audio2.src = '../assets/music/marshall2.mp3';
     audio2.play();
     audio2.addEventListener('ended',handleEnded2);
 };
@@ -20,8 +22,15 @@ const handleEnded = () => {
 const handleEnded2 = () => {
     console.log("scene2 ended");
     scene.src = "../assets/img/scene3.png";
-    audio2.src = '../assets/music/Storyline_final.mp3'
-    audio2.play();
+    audio3.src = '../assets/music/marshall3.mp3'
+    audio3.play();
+};
+
+const handleEnded3 = () => {
+    console.log("scene3 ended = laatste scene");
+    scene.src = "../assets/img/scene4.png";
+    audio4.src = '../assets/music/marshall4.mp3'
+    audio4.play();
 };
 
 aud.addEventListener('ended', handleEnded);
