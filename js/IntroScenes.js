e07ea9912edffa52ce85779db1c12c078af8599d
LisaLapon <lisa.lapon@student.howest.be>
2018-12-17 01:33:21 +0100

    effectje bij scenes , werkt half

--- js/IntroScenes.js
+++ js/IntroScenes.js
@@ -13,8 +13,10 @@ skip.addEventListener("click", function () {
 
 // CHANGE SCENE AFTER DONE
 const handleEnded = () => {
+    
     console.log("scene1 ended");
     scene.src = "../assets/img/scene2.png";
+    scene.classList.add("spin");
 
     audio2.src = '../assets/music/marshall2.mp3';
     audio2.play();
@@ -22,16 +24,21 @@ const handleEnded = () => {
 };
 
 const handleEnded2 = () => {
+    scene.classList.remove("spin");
     console.log("scene2 ended");
     scene.src = "../assets/img/scene3.png";
-    audio3.src = '../assets/music/marshall3.mp3'
+
+    audio3.src = '../assets/music/marshall3.mp3';
     audio3.play();
     audio3.addEventListener('ended',handleEnded3);
 };
 
 const handleEnded3 = () => {
+    scene.classList.add("spin");
     console.log("scene3 ended = laatste scene");
     scene.src = "../assets/img/scene4.png";
+    scene.classList.add("spin");
+
     audio4.src = '../assets/music/marshall4.mp3'
     audio4.play();
     audio4.addEventListener('ended',handleEnded4);
